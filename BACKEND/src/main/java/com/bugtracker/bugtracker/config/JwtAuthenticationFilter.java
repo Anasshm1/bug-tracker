08fbb633c3c033c80dc4c0545d7a9ea9110af048
Anasshm1 <anas.hm7777@gmail.com>
2026-07-02 19:26:58 +0100

    feat: complete authentication system (register, login, JWT)

--- BACKEND/src/main/java/com/bugtracker/bugtracker/config/JwtAuthenticationFilter.java
+++ BACKEND/src/main/java/com/bugtracker/bugtracker/config/JwtAuthenticationFilter.java
@@ -4,8 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,11 +19,15 @@ import java.io.IOException;
 
 @Component
 @Log4j2
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, @Lazy UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(
